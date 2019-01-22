baaf5e54dc62baab1b9627a69c8f9a50f140f970
Evan Canoy <evan@Evans-MacBook-Pro.local>
2019-01-22 11:15:07 -0500

    build models

---
+++ models/recipesModels.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Recipe = new Schema({
+    Recipe: {
+        title: String,
+        description: String,
+        instructions: String,
+        ingredients: [String],
+        //user? 
+    }
+    User: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+module.exports = {
+    Recipe: mongoose.model('Recipe', Recipe)
+}
---
+++ models/usersModels.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    Name: {
+        firstName: String,
+        lastName: String
+    },
+    Recipe: {
+        type: Schema.Types.ObjectId.app,
+        ref: 'Recipe'
+    }
+})
+
+
+module.exports = {
+    User: mongoose.model('User', User),
+
+}
