126640d43c28756a5f21d029c990e49e2cc204a9
Roger Campbell II <rc1336@gmail.com>
2019-01-22 11:48:21 -0500

    create the createRecipe view and viewRecipe view. Creates layout view.

--- models/recipesModels.js
+++ models/recipesModels.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Recipe = new Schema({
-    Recipe: {
-        title: String,
-        description: String,
-        instructions: String,
-        ingredients: [String],
-        //user? 
-    }
+    title: String,
+    description: String,
+    instructions: String,
+    ingredients: [String],
     User: {
         type: Schema.Types.ObjectId,
         ref: 'User'
